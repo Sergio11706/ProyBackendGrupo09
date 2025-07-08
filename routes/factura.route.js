129ffddf894d7a1375a9f019519559d5530b7728
agusToro <agustingastontoro@gmail.com>
2025-07-08 11:41:10 -0300

    Sacando Agente y Factura Routes

--- routes/factura.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const facturaCtrl = require('../controllers/factura.controller');
-
-router.post('/crear', facturaCtrl.crearFactura);
-router.get('/', facturaCtrl.obtenerTodas);
-
-module.exports = router;
